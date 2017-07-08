aa1ff13f2f5395675425e20e3b0c7a099a3b0f6d
Tanuj Garg <Tanujgarg@y7mail.com>
2017-07-08 12:55:03 +0530

    Getting self info

---
+++ InstaBot/instaBot.py
@@ -0,0 +1,24 @@
+import requests
+from termcolor import colored
+App_access_token = '5508711577.1063dec.210e3b7e96f14227b50f96e961f098d6'
+Base_Url = 'https://api.instagram.com/v1/'
+
+
+def self_info():
+    request_url = Base_Url + "users/self/?access_token=" + App_access_token
+    print("Get request url : ",request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print('Username : ',colored(user_info['data']['username'],'blue'))
+            print('No. of followers : ',colored(user_info['data']['counts']['followed_by'],'blue'))
+            print('No. of people you are following : ',colored(user_info['data']['counts']['follows'],'blue'))
+            print('No. of posts : ',colored(user_info['data']['counts']['media'],'blue'))
+        else:
+            print(colored('User does not exist!','red'))
+    else:
+        print(colored('Status code other than 200 received!','blue'))
+
+
+self_info()
